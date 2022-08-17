5ebc8b8e96fe0874ac0dd9c666b4a9a060a2f460
Jaewon Kwak <sovioletta@gmail.com>
2022-08-17 01:11:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def callback():
         # get request body as text
         body = request.get_data(as_text=True)
         app.logger.info("Request body: " + body)
+        print(body)
 
         # handle webhook body
         try:
